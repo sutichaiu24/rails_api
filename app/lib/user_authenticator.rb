8080ca6cf4eb8db259dca9c8b9aa6c993b582531
sudhichai ungsuthornrungsi <sutichai@isecthailand.com>
2018-09-29 13:39:20 +0700

    Authentication success

--- app/lib/user_authenticator.rb
+++ app/lib/user_authenticator.rb
@@ -1,13 +1,29 @@
 class UserAuthenticator
-    class AuthenticationError < StandardError; end
-    attr_reader :user
+  class AuthenticationError < StandardError; end
 
-    def initialize(code)
-    
+  attr_reader :user
+
+  def initialize(code)
+    @code = code
+  end
+
+  def perform
+    client = Octokit::Client.new(
+      client_id: ENV['GITHUB_CLIENT_ID'],
+      client_secret: ENV['GITHUB_CLIENT_SECRET']
+    )
+    token = client.exchange_code_for_token(code)
+    if token.try(:error).present?
+      raise AuthenticationError
+    else
+      user_client = Octokit::Client.new(
+        access_token: token)
+      user_data = user_client.user.to_h.slice(:login, :avatar_url, :url, :name)
+      User.create(user_data.merge(provider: 'github'))
     end
-    
-    def perform
-        raise AuthenticationError
-    end
-    
+  end
+
+  private
+
+  attr_reader :code
 end
