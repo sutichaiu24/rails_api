61ad79fe8629ea392b0bf6351e3672bcac806c42
sudhichai ungsuthornrungsi <sutichai@isecthailand.com>
2018-10-07 19:47:42 +0700

    Unauthorized login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   #access_tokens#creat For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     resources :articles, only: [:index,:show]
+    post 'login', to: 'access_tokens#create' 
 end
